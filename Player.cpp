502cafdaa81eadfc7d7b15300cfb2fc97eccf97e
shahar.ben <shahar.ben@campus.technion.ac.il>
2022-05-06 12:40:53 +0300

    added printInfo,levelUp,getLevel funcs

--- Player.cpp
+++ Player.cpp
@@ -3,6 +3,7 @@
 //
 #include "Player.h"
 
+const int MAX_LEVEL = 10;
 Player::Player(char* name, int HP, int force)
 {
     this->name = allocateAndCopy(name, strlen(name));
@@ -10,4 +11,22 @@ Player::Player(char* name, int HP, int force)
 
 char* Player::allocateAndCopy(const char* str, int size) {
     return strcpy(new char[size + 1], str);
+}
+
+void Player::printInfo()
+{
+    printPlayerInfo(this->name, this->level, this->force, this->HP, this->coins);
+}
+
+void Player::levelUp()
+{
+    if (this->level<MAX_LEVEL)
+    {
+        this->level++;
+    }
+}
+
+int Player::getLevel()
+{
+    return this->level;
 }
