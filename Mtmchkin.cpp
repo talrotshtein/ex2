4059193eee2c828afee37b9195a29ab056a03e98
shahar.ben <shahar.ben@campus.technion.ac.il>
2022-05-07 12:52:22 +0300

    added Mtmchkin funcs

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -6,3 +6,25 @@
 Mtmchkin::Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCards) {
     Player player1(playerName);
 }
+
+GameStatus Mtmchkin::getGameStatus() const
+{
+    return this->m_status;
+}
+
+void Mtmchkin::playNextCard()
+{
+
+}
+
+bool Mtmchkin::isOver()
+{
+    if(this->m_status == GameStatus::MidGame)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
